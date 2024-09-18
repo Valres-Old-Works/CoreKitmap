e64e3d99a096541f394c843cb601c2da5d2dead9
Valres <tsterle59@gmail.com>
2024-09-18 14:58:32 +0200

    Update IconComponent.php

--- src/Valres/CoreKitmap/libs/customies/item/component/IconComponent.php
+++ src/Valres/CoreKitmap/libs/customies/item/component/IconComponent.php
@@ -6,9 +6,11 @@ namespace Valres\CoreKitmap\libs\customies\item\component;
 final class IconComponent implements ItemComponent {
 
 	private string $texture;
+    private ?string $iconTrim;
 
-	public function __construct(string $texture) {
+	public function __construct(string $texture, ?string $iconTrim = null) {
 		$this->texture = $texture;
+        $this->iconTrim = $iconTrim;
 	}
 
 	public function getName(): string {
@@ -16,11 +18,15 @@ final class IconComponent implements ItemComponent {
 	}
 
 	public function getValue(): array {
-		return [
+		$array = [
 			"textures" => [
 				"default" => $this->texture
 			]
 		];
+        if(!is_null($this->iconTrim)){
+            $array["textures"]["icon_trim"] = $this->iconTrim;
+        }
+        return $array;
 	}
 
 	public function isProperty(): bool {
