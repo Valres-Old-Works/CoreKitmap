a81c601ae825f46de732c2d868f0551392dea025
Valres <tsterle59@gmail.com>
2024-09-18 17:05:31 +0200

    init grades

---
+++ src/Valres/CoreKitmap/managers/grades/GradesManager.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ *
+ *   ____               _  ___ _
+ *  / ___|___  _ __ ___| |/ (_) |_ _ __ ___   __ _ _ __
+ * | |   / _ \| '__/ _ \ ' /| | __| '_ ` _ \ / _` | '_ \
+ * | |__| (_) | | |  __/ . \| | |_| | | | | | (_| | |_) |
+ *  \____\___/|_|  \___|_|\_\_|\__|_| |_| |_|\__,_| .__/
+ *                                                |_|
+ * ENG: This file is strictly confidential and personal.
+ * It contains code developed for private purposes and must not be distributed, shared or used without the explicit permission of the author.
+ * Any violation will be subject to legal action.
+ * FRA: Ce fichier est strictement confidentiel et personnel.
+ * Il contient du code développé à des fins privées et ne doit en aucun cas être distribué, partagé ou utilisé sans autorisation explicite de l'auteur.
+ * Toute violation sera passible de poursuites légales.
+ *
+ * @author ValresMC
+ * @version v0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace Valres\CoreKitmap\managers\grades;
+
+use JsonException;
+use pocketmine\utils\Config;
+use Valres\CoreKitmap\managers\BaseManager;
+
+class GradesManager extends BaseManager
+{
+    /** @var Grade[] */
+    private array $grades = [];
+    private Config $datas;
+
+    public function getName(): string {
+        return "Grades";
+    }
+
+    public function load(): void {
+        $this->datas = $this->getPlugin()->getConfigFile("grades/grades.yml");
+
+        foreach($this->datas->getAll() as $name => $data){
+            $this->grades[$name] = new Grade($name, $data["displayName"], $data["chatFormat"], $data["nametagFormat"], $data["color"], $data["permissions"]);
+        }
+    }
+
+    /** @throws JsonException */
+    public function save(): void {
+        foreach($this->grades as $name => $data){
+            $this->datas->set($name, [
+                "displayName" => $data->getDisplayName(),
+                "chatFormat" => $data->getChatFormat(),
+                "nametagFormat" => $data->getNametagFormat(),
+                "color" => $data->getColor(),
+                "permissions" => $data->getPermissions()
+            ]);
+        }
+        $this->datas->save();
+    }
+
+    public function addGrade(Grade $grade): void {
+        $this->grades[] = $grade;
+    }
+
+    public function removeGrade(string $name): void {
+        unset($this->grades[array_search($name, $this->grades)]);
+    }
+
+    public function getGrades(): array {
+        return $this->grades;
+    }
+
+    public function getGrade(string $name): ?Grade {
+        return $this->grades[$name] ?? null;
+    }
+
+    public function exist(string $name): bool {
+        return array_key_exists($name, $this->grades);
+    }
+}
