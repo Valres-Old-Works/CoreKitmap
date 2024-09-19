69e67c4e988106bb1c5694836ad3158724bcfe03
Valres <tsterle59@gmail.com>
2024-09-19 02:06:21 +0200

    Stats system

---
+++ src/Valres/CoreKitmap/managers/combat/Statistics.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ *   ____               _  ___ _
+ *  / ___|___  _ __ ___| |/ (_) |_ _ __ ___   __ _ _ __
+ * | |   / _ \| '__/ _ \ ' /| | __| '_ ` _ \ / _` | '_ \
+ * | |__| (_) | | |  __/ . \| | |_| | | | | | (_| | |_) |
+ *  \____\___/|_|  \___|_|\_\_|\__|_| |_| |_|\__,_| .__/
+ *                                                |_|
+ * ENG: This file is strictly confidential and personal.
+ * It contains code developed for private purposes and must not be distributed, shared or used without the explicit permission of the author.
+ * Any violation will be subject to legal action.
+ * FRA: Ce fichier est strictement confidentiel et personnel.
+ * Il contient du code développé à des fins privées et ne doit en aucun cas être distribué, partagé ou utilisé sans autorisation explicite de l'auteur.
+ * Toute violation sera passible de poursuites légales.
+ *
+ * @author ValresMC
+ * @version v0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace Valres\CoreKitmap\managers\combat;
+
+class Statistics
+{
+    public function __construct(
+        protected int $kills = 0,
+        protected int $death = 0
+    ){}
+
+    public function getKills(): int {
+        return $this->kills;
+    }
+
+    public function getDeath(): int {
+        return $this->death;
+    }
+
+    public function getKDR(): float {
+        return $this->death === 0 ? $this->kills : round($this->kills / $this->death, 2);
+    }
+}
---
+++ src/Valres/CoreKitmap/managers/combat/StatisticsManager.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ *
+ *   ____               _  ___ _
+ *  / ___|___  _ __ ___| |/ (_) |_ _ __ ___   __ _ _ __
+ * | |   / _ \| '__/ _ \ ' /| | __| '_ ` _ \ / _` | '_ \
+ * | |__| (_) | | |  __/ . \| | |_| | | | | | (_| | |_) |
+ *  \____\___/|_|  \___|_|\_\_|\__|_| |_| |_|\__,_| .__/
+ *                                                |_|
+ * ENG: This file is strictly confidential and personal.
+ * It contains code developed for private purposes and must not be distributed, shared or used without the explicit permission of the author.
+ * Any violation will be subject to legal action.
+ * FRA: Ce fichier est strictement confidentiel et personnel.
+ * Il contient du code développé à des fins privées et ne doit en aucun cas être distribué, partagé ou utilisé sans autorisation explicite de l'auteur.
+ * Toute violation sera passible de poursuites légales.
+ *
+ * @author ValresMC
+ * @version v0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace Valres\CoreKitmap\managers\combat;
+
+use pocketmine\utils\Config;
+use Valres\CoreKitmap\managers\BaseManager;
+
+class StatisticsManager extends BaseManager
+{
+    /** @var Statistics[] */
+    private array $stats = [];
+
+    private Config $datas;
+
+
+    public function getName(): string {
+        return "Statistics";
+    }
+
+    public function load(): void {
+        // TODO: Implement load() method.
+    }
+
+    public function save(): void {
+        // TODO: Implement save() method.
+    }
+
+    public function getStats(string $playerName): ?Statistics {
+        return $this->stats[$playerName] ?? null;
+    }
+
+    public function getKills(): array {
+        return array_map(function(Statistics $stats): int {
+            return $stats->getKills();
+        }, $this->stats);
+    }
+
+    public function getDeaths(): array {
+        return array_map(function(Statistics $stats): int {
+            return $stats->getDeath();
+        }, $this->stats);
+    }
+
+    public function getKDR(): array {
+        return array_map(function(Statistics $stats): float {
+            return $stats->getKDR();
+        }, $this->stats);
+    }
+
+    public function exist(string $playerName): bool {
+        return array_key_exists($playerName, $this->stats);
+    }
+
+    public function register(string $playerName): void {
+        $this->stats[$playerName] = new Statistics();
+    }
+}
